a2cc652bebbb453c9bdcdf32102a67541581696f
KENNETH-AIDAN-B <kennethaidanmarvellous@gmail.com>
2025-06-13 18:35:19 +0530

    Create Dashboard.js

---
+++ admin/src/pages/Dashboard.js
@@ -0,0 +1,15 @@
+// admin/src/pages/Dashboard.js
+
+import React from 'react';
+import PricingConfig from '../components/PricingConfig';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <PricingConfig />
+    </div>
+  );
+};
+
+export default Dashboard;
